2f6d3b5a780e274e810082c33e9f89a6e70600fa
jackson.lu02@myhunter.cuny.edu <jackson.lu02@myhunter.cuny.edu>
2021-08-21 21:29:19 -0400

    fixed background in desktop size

--- src/components/Background.js
+++ src/components/Background.js
@@ -5,7 +5,10 @@ import mobileherobg from "../images/illustration-woman-online-mobile.svg"
 const Background = () => {
 	return (
 		<div className="bg">
-			<img className="bg-hero" src={mobileherobg} alt="background hero" />
+            <div className="bg-top">
+                <img className="bg-hero" src={mobileherobg} alt="background hero" />
+            </div>
+			
 			<img className="bg-blur" src={mobilepatternbg} alt="background blur" />
 		</div>
 	);
